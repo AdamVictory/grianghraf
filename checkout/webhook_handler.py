775985804edfcdbb1bc6227f90eab30ca9858dc7
AdamVictory <adam.victory1998@gmail.com>
2023-03-20 15:52:07 +0000

    Added correct stripe secret

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -16,8 +16,8 @@ class StripeWH_Handler:
     def __init__(self, request):
         self.request = request
 
-    def _send_confirmation_email(self, order): 
-        """ Send the user a confirmation email"""
+    def _send_confirmation_email(self, order):
+        """Send the user a confirmation email"""
         cust_email = order.email
         subject = render_to_string(
             'checkout/confirmation_emails/confirmation_email_subject.txt',
