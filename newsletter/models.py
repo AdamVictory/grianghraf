5756637aed2d5ba1f9cac8fbae930d78d6382296
AdamVictory <adam.victory1998@gmail.com>
2023-03-21 18:36:58 +0000

    newsletter

---
+++ newsletter/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class SubscribedUsers(models.Model): 
+    """Entity to subscribed users to newsletter"""
+    email = models.CharField(unique=True, max_length=50)
+    name = models.CharField(max_length=50)
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE, 
+        related_name="user", null=True
+        )
