60cb810d0773e1649311690aad0d225aa9d7c5ac
AdamVictory <adam.victory1998@gmail.com>
2023-03-21 12:54:25 +0000

    f

--- grianghraf/urls.py
+++ grianghraf/urls.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import handler404
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -14,4 +13,3 @@ urlpatterns = [
     path('profile/', include('profiles.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-HANDLER404 = "grianghraf.views.handler404"
