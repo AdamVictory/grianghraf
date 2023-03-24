7738310d831879a8d7e9b23d96bf1727e6467e72
AdamVictory <adam.victory1998@gmail.com>
2023-03-24 15:12:47 +0000

    heroku

--- grianghraf/settings.py
+++ grianghraf/settings.py
@@ -1,5 +1,7 @@
 import os
 from pathlib import Path
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -14,7 +16,7 @@ SECRET_KEY = 'django-insecure-f1aixtz(0%8f&o@zua73ntjp55uz2!^a-ly9$9+pn=wb(p04s!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['grianghraf.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -110,17 +112,20 @@ LOGIN_REDIRECT_URL = '/'
 
 WSGI_APPLICATION = 'grianghraf.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
-
+else:
+    DATABASES = {
+        'default': {
+          'ENGINE': 'django.db.backends.sqlite3',
+          'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
