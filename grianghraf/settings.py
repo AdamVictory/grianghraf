2cf01c7af2e378121f3ae72bf40919e86829b93b
AdamVictory <adam.victory1998@gmail.com>
2023-03-24 15:44:12 +0000

    heroku

--- grianghraf/settings.py
+++ grianghraf/settings.py
@@ -11,12 +11,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-f1aixtz(0%8f&o@zua73ntjp55uz2!^a-ly9$9+pn=wb(p04s!'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['grianghraf.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['grianghraf.herokuapp.com', 'localhost' ]
 
 
 # Application definition
