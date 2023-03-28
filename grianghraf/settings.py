ba902d3f3f848c84da9872ab85eaf543235e4f83
AdamVictory <adam.victory1998@gmail.com>
2023-03-28 14:05:35 +0000

    gmail

--- grianghraf/settings.py
+++ grianghraf/settings.py
@@ -200,9 +200,8 @@ STRIPE_CURRENCY = 'usd'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
-DEFAULT_FROM_EMAIL = 'grianghraf@example.com'
+DEFAULT_FROM_EMAIL = 'grianghraf123@gmail.com'
 
-# Gmail settings
 if "DEVELOPMENT" in os.environ:
     EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
     DEFAULT_FROM_EMAIL = "grianghraf@example.com"
