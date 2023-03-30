47ec27e2f8d7639f9ed98482084dde06079fd766
AdamVictory <adam.victory1998@gmail.com>
2023-03-30 12:15:22 +0000

    changed endpoint

--- grianghraf/settings.py
+++ grianghraf/settings.py
@@ -14,8 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = 'DEVELOPMENT' in os.environ
-DEBUG = True 
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['grianghraf.herokuapp.com', 'localhost']
 
@@ -203,17 +202,17 @@ STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 DEFAULT_FROM_EMAIL = 'grianghraf123@gmail.com'
  
-# if "DEVELOPMENT" in os.environ:
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
-DEFAULT_FROM_EMAIL = "grianghraf@example.com"
-# else:
-#   EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-#   EMAIL_USE_TLS = True
-#   EMAIL_PORT = 587
-#   EMAIL_HOST = "smtp.gmail.com"
-#   EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
-#   EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASS")
-#   DEFAULT_FROM_EMAIL = os.environ.get("EMAIL_HOST_USER")
+if "DEVELOPMENT" in os.environ:
+    EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+    DEFAULT_FROM_EMAIL = "grianghraf@example.com"
+else:
+    EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = "smtp.gmail.com"
+    EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+    EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASS")
+    DEFAULT_FROM_EMAIL = os.environ.get("EMAIL_HOST_USER")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
